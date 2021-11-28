3d027ab837242586749c3c9fc66c7e7270f318b3
Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>
2021-11-27 22:07:47 -0500

    Adjust test classes to be able to perform mutation testing

---
+++ assignments/assignment 2/eecs4313a2b/src/eecs4313a2b/BoundaryValueTest.java
@@ -0,0 +1,103 @@
+package eecs4313a2b;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Reuben Ninan (216315509)
+ * 
+ */
+public class BoundaryValueTest {
+
+	Util u;
+
+	@Before
+	public void setUp() throws Exception {
+		u = new Util();
+	}
+	
+	@Test
+	public void test_d1_nominal_d2_min() {
+		// d1 nominal value = current time 
+		Date d1 = new Date();
+		Date d2 = new Date(0);
+
+		Assert.assertTrue(Util.isAfter(d1, d2));
+	}
+	
+	@Test
+	public void test_d1_nominal_d2_minplus() {
+		// d1 nominal value = current time 
+		Date d1 = new Date();
+		Date d2 = new Date(1);;
+
+		Assert.assertTrue(Util.isAfter(d1, d2));
+	}
+	
+	@Test
+	public void test_d1_nominal_d2_nominal() {
+		// d1 nominal value = current time 
+		Date d1 = new Date();
+		Date d2 = new Date(System.currentTimeMillis());;
+
+		Assert.assertFalse(Util.isAfter(d1, d2));
+	}
+	
+	@Test
+	public void test_d1_nominal_d2_maxminus() {
+		// d1 nominal value = current time 
+		Date d1 = new Date();
+		Date d2 = new Date(Long.MAX_VALUE - 1);;
+
+		Assert.assertFalse(Util.isAfter(d1, d2));
+	}
+	
+	@Test
+	public void test_d1_nominal_d2_max() {
+		// d1 nominal value = current time 
+		Date d1 = new Date();
+		Date d2 = new Date(Long.MAX_VALUE);
+
+		Assert.assertFalse(Util.isAfter(d1, d2));
+	}
+	
+	@Test
+	public void test_d1_min_d2_nominal() {
+		// d1 nominal value = current time 
+		Date d1 = new Date(0);
+		Date d2 = new Date();
+
+		Assert.assertFalse(Util.isAfter(d1, d2));
+	}
+	
+	@Test
+	public void test_d1_minplus_d2_nominal() {
+		// d1 nominal value = current time 
+		Date d1 = new Date(1);
+		Date d2 = new Date();
+
+		Assert.assertFalse(Util.isAfter(d1, d2));
+	}
+	
+	@Test
+	public void test_d1_maxminus_d2_nominal() {
+		// d1 nominal value = current time 
+		Date d1 = new Date(Long.MAX_VALUE - 1);
+		Date d2 = new Date();
+
+		Assert.assertTrue(Util.isAfter(d1, d2));
+	}
+	
+	@Test
+	public void test_d1_max_d2_nominal() {
+		// d1 nominal value = current time 
+		Date d1 = new Date(Long.MAX_VALUE);
+		Date d2 = new Date();
+
+		Assert.assertTrue(Util.isAfter(d1, d2));
+	}
+
+}
--- assignments/assignment 2/eecs4313a2b/src/eecs4313a2b/DecisionTableTest.java
+++ assignments/assignment 2/eecs4313a2b/src/eecs4313a2b/DecisionTableTest.java
@@ -1,13 +1,9 @@
 package eecs4313a2b;
 
+
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import net.sf.borg.common.DateUtil;
-
-
 
 /**
  * @author Ahmed Hagi (215043896)
@@ -15,66 +11,50 @@ import net.sf.borg.common.DateUtil;
  */
 
 public class DecisionTableTest {
-	
-	DateUtil d;
-
-	@Before
-	public void setUp() throws Exception {
-		DateUtil d = new DateUtil();
-	}
 
 	@Test
 	public void H1_M1() {
-		String s = d.minuteString(0);
-		assertTrue(s.equals("0 Minutes"));
+		assertTrue(Util.minuteString(0).equals("0 Minutes"));
 	}
 	
 	@Test
 	public void H1_M2() {
-		String s = d.minuteString(1);
-		assertTrue(s.equals("1 Minute"));
+		assertTrue(Util.minuteString(1).equals("1 Minute"));
 	}
 	
 	@Test
 	public void H1_M3() {
-		String s = d.minuteString(37);
-		assertTrue(s.equals("37 Minutes"));
+		assertTrue(Util.minuteString(37).equals("37 Minutes"));
 	}
 	
 	@Test
 	public void H2_M1() {
-		String s = d.minuteString(60);
-		assertTrue(s.equals("1 Hour"));
+		assertTrue(Util.minuteString(60).equals("1 Hour"));
 	}
 	
 	@Test
 	public void H2_M2() {
-		String s = d.minuteString(61);
-		assertTrue(s.equals("1 Hour 1 Minute"));
+		assertTrue(Util.minuteString(61).equals("1 Hour 1 Minute"));
 	}
 	
 	@Test
 	public void H2_M3() {
-		String s = d.minuteString(86);
-		assertTrue(s.equals("1 Hour 26 Minutes"));
+		assertTrue(Util.minuteString(86).equals("1 Hour 26 Minutes"));
 	}
 	
 	@Test
 	public void H3_M1() {
-		String s = d.minuteString(660);
-		assertTrue(s.equals("11 Hours"));
+		assertTrue(Util.minuteString(660).equals("11 Hours"));
 	}
 	
 	@Test
 	public void H3_M2() {
-		String s = d.minuteString(361);
-		assertTrue(s.equals("6 Hours 1 Minute"));
+		assertTrue(Util.minuteString(361).equals("6 Hours 1 Minute"));
 	}
 	
 	@Test
 	public void H3_M3() {
-		String s = d.minuteString(988);
-		assertTrue(s.equals("16 Hours 28 Minutes"));
+		assertTrue(Util.minuteString(988).equals("16 Hours 28 Minutes"));
 	}
 
 }
